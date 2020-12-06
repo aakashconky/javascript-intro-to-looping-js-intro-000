1ff82a2b8419dd2c2ae389532b3a746e887e837f
aakash thapa <aakash_conky@hotmail.com>
2020-12-06 02:24:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,9 +9,9 @@ array.push('I am ${i} strange loop.');
 return array;
 }
 
-function whileLoop(number) {
-while (number > 0) {
-console.log(–number);
+function whileLoop(n) {
+while (n > 0) {
+console.log(–-n);
 }
 return “done”;
 }
